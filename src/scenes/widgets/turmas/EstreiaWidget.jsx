cd9c2b112f4e0d96ce1dad7df25905bbec5db9aa
Felipe Brevio <felipebrevio05@gmail.com>
2023-11-17 21:05:46 -0300

    feat countdownTimer

--- src/scenes/widgets/turmas/EstreiaWidget.jsx
+++ src/scenes/widgets/turmas/EstreiaWidget.jsx
@@ -1,8 +1,9 @@
+import CountdownTimer from "components/countdownTimer/CountdownTimer";
 import PostComponent from "../../../components/post/PostComponent";
 
 const EstreiaWidget = () => {
 
-    return <PostComponent titulo={"Contagem regressiva..."} subtitulo={"Faltam poucos dias para grande estreia"} content={"11 dias"} />
+        return <PostComponent titulo={"Contagem regressiva..."} subtitulo={"Faltam poucos dias para grande estreia"} content={<CountdownTimer targetDate={"2023-11-27T19:00:00.000+00:00"} />} />
 }
 
 export default EstreiaWidget;
