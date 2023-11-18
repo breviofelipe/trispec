4fad3cddb622b2dbdcd03a771bc152155fc86bc4
Felipe Brevio <felipebrevio05@gmail.com>
2023-11-17 22:26:51 -0300

    feature Contagem regressiva

--- src/scenes/widgets/turmas/EstreiaWidget.jsx
+++ src/scenes/widgets/turmas/EstreiaWidget.jsx
@@ -4,7 +4,7 @@ import { FaTheaterMasks } from 'react-icons/fa';
 
 const EstreiaWidget = () => {
 
-        return <PostComponent icon={<FaTheaterMasks size={36}/>} titulo={"Contagem regressiva..."} subtitulo={"Faltam poucos dias para grande estreia"} content={<CountdownTimer targetDate={"2023-11-27T19:00:00.000+00:00"} />} />
+        return <PostComponent icon={<FaTheaterMasks size={36}/>} titulo={"Contagem regressiva..."} subtitulo={"Faltam poucos dias para a grande estreia"} content={<CountdownTimer targetDate={"2023-11-27T19:00:00.000+00:00"} />} />
 }
 
 export default EstreiaWidget;
